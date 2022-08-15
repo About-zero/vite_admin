6a29547062c98f3c74340c5d7ba4f13e2d7c5d90
lxp <1273261054@qq.com>
2022-08-15 21:28:15 +0800

    @语法

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  // 使用@引用
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    // ip地址
+    host: "localhost",
+    // 端口
+    port: 9090,
+    open: true,
+    // 热更新
+    hmr: true,
+  },
+});
